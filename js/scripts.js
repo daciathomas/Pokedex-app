b2679d54ebf23f170b2105b95604a528f0f1e57c
daciathomas <60546806+daciathomas@users.noreply.github.com>
2020-05-14 01:03:12 -0400

    Added var pokemonList & add objects to arrays in script.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,27 @@
-var pokemonList = [];
+var pokemonList = [
+  {
+    name: 'Charmeleon',
+    height: 3.07,
+    types: ['fire'],
+    category: ['flame'],
+  },
+  {
+    name: 'Squirtle',
+    height: 1.08,
+    types: ['water'],
+    category: ['tiny turtle'],
+  },
+  {
+    name: 'Caterpie',
+    height: 1. 00,
+    types: ['bug'],
+    category: ['worm'],
+  },
+  {
+    name: 'Venusaur',
+    height: 6.07,
+    types: ['grass','poison'],
+    category: ['seed'],
+  },
+  ];
+  console.log(pokemonList)
