d94eca26c697a3b1365b701473e81a0ae1dc27ca
daciathomas <60546806+daciathomas@users.noreply.github.com>
2020-05-27 06:29:24 -0400

    wrapped array in IIFE and return an add functions with key value pairs

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// wrapping pokemonList array in IIFE in order to directly access from the outside
+var pokemonRepository = (function () {
 var pokemonList = [
   {
     name: 'Charmeleon',
@@ -23,10 +25,30 @@ var pokemonList = [
     types: ['grass','poison'],
     category: ['seed'],
   }
-  ];
+];
 
-//  for (var i = 0; i < pokemonList.length; i++) {
-//   document.write('<h2>' + pokemonList[i].name + '</h2>' + 'height: ' + pokemonList[i].height)
-  pokemonList.forEach(function(creature) {
-  console.log(creature.name + ' height: ' + creature.height);
+  //add a single pokemon attributeto the pokemonList array,
+  function add (pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  // return the array value of Pokemon being pushed by the 'add() function'
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+ };
+})();
+console.log(pokemonRepository.getAll ());
+
+var newPokemon = {name:'Metapod', height: 2.04, types: ['bug'], category: ['cocoon'] };
+pokemonRepository.add(newPokemon);
+
+console.log(pokemonRepository.getAll());
+
+pokemonRepository.getAll().forEach(function(pokemon){
+  document.write(pokemon.name + ' height: ' + pokemon.height);
 });
