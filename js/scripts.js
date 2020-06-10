f320d60d870538c66e3804e578d309f36c2f6517
daciathomas <60546806+daciathomas@users.noreply.github.com>
2020-06-10 12:03:31 -0400

    cut code from for Each loop and move to addList Item function

--- js/scripts.js
+++ js/scripts.js
@@ -39,23 +39,7 @@ function getAll() {
   }
 
 // function to add list for each pokemon
-  function addListeItem(pokemon){
-
-  return { //return all previous function so that it's available outside IIFE
-    add: add,
-    getAll: getAll,
-
-    };
-
-})();
-
-console.log(pokemonRepository.getAll ());
-var newPokemon = {name:'Metapod', height: 2.04, types: ['bug'], category: ['cocoon'] };
-pokemonRepository.add(newPokemon);
-console.log(pokemonRepository.getAll());
-
-//empty for EAch loop block
-pokemonRepository.getAll().forEach(function(pokemon){
+function addListeItem(pokemon){
 // create a variable for the forEach loop then  assign it the ul element
 var pokemonList = document.querySelector('.pokemon-List');
 //create a li element
@@ -71,6 +55,22 @@ listItem.appendChild(button);
 pokemonList.appendChild(listItem);
 
 }
+return { //return all previous function so that it's available outside IIFE
+  add: add,
+  getAll: getAll,
+
+};
+
+})();
+
+console.log(pokemonRepository.getAll ());
+var newPokemon = {name:'Metapod', height: 2.04, types: ['bug'], category: ['cocoon'] };
+pokemonRepository.add(newPokemon);
+console.log(pokemonRepository.getAll());
+
+//empty for EAch loop block
+pokemonRepository.getAll().forEach(function(pokemon){
+
 
 
 
