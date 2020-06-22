f49831bfb0e0b5fa3433fdfda3890f59290d4c76
daciathomas <60546806+daciathomas@users.noreply.github.com>
2020-06-22 14:49:01 -0400

    Removed Pokemon object array

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,7 @@
 
 // wrapping pokemonList array in IIFE in order to directly access from the outside
 var pokemonRepository = (function () {
-var pokemonList = [
-  {
-    name: 'Charmeleon',
-    height: 3.07,
-    types: ['fire'],
-    category: ['flame'],
-  },
-  {
-    name: 'Squirtle',
-    height: 1.08,
-    types: ['water'],
-    category: ['tiny turtle'],
-  },
-  {
-    name: 'Caterpie',
-    height: 1.00,
-    types: ['bug'],
-    category: ['worm'],
-  },
-  {
-    name: 'Venusaur',
-    height: 6.07,
-    types: ['grass','poison'],
-    category: ['seed'],
-  }
-];
+var pokemonList = [ ];
 
 //add a single pokemon attribute to the pokemonList array,
 function add (pokemon) {
